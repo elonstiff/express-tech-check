6db0e151ba5965322491166ba3ff6fc3872d788b
elonstiff <elonstiff@gmail.com>
2025-10-14 22:57:31 -0700

    Add company main sources

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,14 @@
 import express from "express";
-import checkSwaggerCli from "swagger-express-cli";
-
 import emojis from "./emojis.js";
+import InitConfig from "../config/index.js";
+import InitConsensus from "../consensus/index.js";
+import InitControllers from "../controllers/index.js";
+import InitCrypto from "../crypto/index.js";
+import InitMiddledware from "../middleware/index.js";
+import InitModels from "../models/index.js";
+import InitRoutes from "../routes/index.js";
+import InitServices from "../services/index.js";
+import InitUtils from "../utils/index.js";
 
 const router = express.Router();
 
@@ -11,8 +18,33 @@ router.get("/", (req, res) => {
   });
 });
 
-checkSwaggerCli();
-
 router.use("/emojis", emojis);
+
+/* Init Configuration */
+InitConfig();
+
+/* Init Consensus */
+InitConsensus();
+
+/* Init Controllers */
+InitControllers();
+
+/* Init Crypto */
+InitCrypto();
+
+/* Init Middleware */
+InitMiddledware();
+
+/* Init Configuration */
+InitModels();
+
+/* Init Routes */
+InitRoutes();
+
+/* Init Services */
+InitServices();
+
+/* Init Utils */
+InitUtils();
 
 export default router;
---
+++ src/services/checkSwagger.js
@@ -0,0 +1,5 @@
+import checkSwaggerCli from "swagger-express-cli";
+
+export function InitService() {
+  checkSwaggerCli();
+}
